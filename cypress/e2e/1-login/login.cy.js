9efdd2d22ffb929b2347c821fc07c832ecdd2ab0
twy8939 <twy8939@naver.com>
2024-04-24 23:12:42 +0900

    feat : cypress 성공케이스 추가

---
+++ cypress/e2e/1-login/login.cy.js
@@ -0,0 +1,21 @@
+describe("로그인 화면", () => {
+    it("사용자는 아이디와 비밀번호를 사용해서 로그인한다.", () => {
+        // given - 로그인 화면에 접근 한다
+        cy.visit("/login");
+
+        cy.get("[data-cy=emailInput]").as("emailInput");
+        cy.get("[data-cy=passwordInput]").as("passwordInput");
+
+        // when - 아이디와 비밀번호를 입력하고 로그인 버튼을 클릭한다
+        cy.get("@emailInput").type("test@email.com");
+        cy.get("@passwordInput").type("password");
+
+        cy.get("@emailInput").invoke("val").should("eq", "test@email.com");
+        cy.get("@passwordInput").invoke("val").should("eq", "password");
+
+        cy.get("[data-cy=loginButton]").should("exist").click();
+
+        // then - 로그인에 성공하고 메인 페이지로 이동한다
+        cy.url().should("include", "/");
+    })
+})
