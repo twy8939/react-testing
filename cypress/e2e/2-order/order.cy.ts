b1a755105e7cc0c49b108ec7946ad3fd2bd60896
twy8939 <twy8939@naver.com>
2024-04-27 20:17:51 +0900

    feat : food list > request mocking

--- cypress/e2e/2-order/order.cy.ts
+++ cypress/e2e/2-order/order.cy.ts
@@ -8,4 +8,67 @@ describe("template spec", () => {
     cy.get("@deliveryBtn").click();
     cy.url().should("include", "/food-type");
   });
+
+  it("사용자는 음식 종류를 선택할 수 있다.", () => {
+    cy.visit("/food-type");
+
+    cy.intercept(
+      {
+        method: "GET",
+        url: "/restaurant/food-type",
+      },
+      [
+        {
+          id: 1,
+          name: "피자",
+          icon: "https://kr.object.ncloudstorage.com/icons/ic-pizza.png",
+        },
+        {
+          id: 2,
+          name: "동남아",
+          icon: "https://kr.object.ncloudstorage.com/icons/ic-asian.png",
+        },
+        {
+          id: 3,
+          name: "햄버거",
+          icon: "https://kr.object.ncloudstorage.com/icons/ic-burger.png",
+        },
+        {
+          id: 4,
+          name: "디저트",
+          icon: "https://kr.object.ncloudstorage.com/icons/ic-cake.png",
+        },
+        {
+          id: 5,
+          name: "치킨",
+          icon: "https://kr.object.ncloudstorage.com/icons/ic-chicken.png",
+        },
+        {
+          id: 6,
+          name: "탕,찌개",
+          icon: "https://kr.object.ncloudstorage.com/icons/ic-hotpot.png",
+        },
+        {
+          id: 7,
+          name: "고기",
+          icon: "https://kr.object.ncloudstorage.com/icons/ic-meat.png",
+        },
+        {
+          id: 8,
+          name: "중식",
+          icon: "https://kr.object.ncloudstorage.com/icons/ic-noodle.png",
+        },
+        {
+          id: 9,
+          name: "샐러드",
+          icon: "https://kr.object.ncloudstorage.com/icons/ic-salad.png",
+        },
+      ]
+    );
+
+    cy.get("[data-cy=1]").should("be.visible").as("pizzaBtn");
+    cy.get("@pizzaBtn").click();
+
+    cy.url().should("include", "/food-type/1");
+  });
 });
