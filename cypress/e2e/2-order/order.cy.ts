960232b5f4d95d3614dd3b19f65338ee2b0e2d38
twy8939 <twy8939@naver.com>
2024-04-28 00:48:05 +0900

    feat : fixture 적용

--- cypress/e2e/2-order/order.cy.ts
+++ cypress/e2e/2-order/order.cy.ts
@@ -17,58 +17,36 @@ describe("template spec", () => {
         method: "GET",
         url: "/restaurant/food-type",
       },
-      [
-        {
-          id: 1,
-          name: "피자",
-          icon: "https://kr.object.ncloudstorage.com/icons/ic-pizza.png",
-        },
-        {
-          id: 2,
-          name: "동남아",
-          icon: "https://kr.object.ncloudstorage.com/icons/ic-asian.png",
-        },
-        {
-          id: 3,
-          name: "햄버거",
-          icon: "https://kr.object.ncloudstorage.com/icons/ic-burger.png",
-        },
-        {
-          id: 4,
-          name: "디저트",
-          icon: "https://kr.object.ncloudstorage.com/icons/ic-cake.png",
-        },
-        {
-          id: 5,
-          name: "치킨",
-          icon: "https://kr.object.ncloudstorage.com/icons/ic-chicken.png",
-        },
-        {
-          id: 6,
-          name: "탕,찌개",
-          icon: "https://kr.object.ncloudstorage.com/icons/ic-hotpot.png",
-        },
-        {
-          id: 7,
-          name: "고기",
-          icon: "https://kr.object.ncloudstorage.com/icons/ic-meat.png",
-        },
-        {
-          id: 8,
-          name: "중식",
-          icon: "https://kr.object.ncloudstorage.com/icons/ic-noodle.png",
-        },
-        {
-          id: 9,
-          name: "샐러드",
-          icon: "https://kr.object.ncloudstorage.com/icons/ic-salad.png",
-        },
-      ]
+      {
+        fixture: "food-type-list.json",
+      }
     );
 
     cy.get("[data-cy=1]").should("be.visible").as("pizzaBtn");
     cy.get("@pizzaBtn").click();
 
     cy.url().should("include", "/food-type/1");
+  });
+
+  it("사용자는 원하는 레스토랑을 선택할 수 있다.", () => {
+    cy.visit("/food-type/1");
+    cy.intercept(
+      {
+        method: "GET",
+        url: "/restaurant/food-type/1",
+      },
+      {
+        fixture: "restaurant-list.json",
+      }
+    );
+
+    cy.fixture("restaurant-list.json").then((restaurantList) => {
+      cy.get(`[data-cy=${restaurantList[0].id}]`)
+        .should("be.visible")
+        .as("restaurantBtn");
+      cy.get("@restaurantBtn").click();
+
+      cy.url().should("include", "/restaurant/1");
+    });
   });
 });
