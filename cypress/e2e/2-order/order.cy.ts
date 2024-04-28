7782c509d2e2c11995f16d57f661ed27362dd0cc
twy8939 <twy8939@naver.com>
2024-04-29 01:09:28 +0900

    feat : order 기능 테스트 적용

--- cypress/e2e/2-order/order.cy.ts
+++ cypress/e2e/2-order/order.cy.ts
@@ -49,4 +49,33 @@ describe("template spec", () => {
       cy.url().should("include", "/restaurant/1");
     });
   });
+
+  it("사용자는 원하는 메뉴를 장바구니에 담고, 원하는 음식 갯수를 변경할 수 있다.", () => {
+    cy.visit("/restaurant/1");
+    cy.fixture("menu.json").then((menu) => {
+      cy.get(`[data-cy=${menu.menu_set[0].id}]`)
+        .should("be.visible")
+        .as("foodBtn");
+
+      cy.get("@foodBtn").click();
+
+      cy.url().should("include", "/order");
+      cy.get(`[data-cy=counter]`).as("couetr");
+      cy.get("@couetr").should("contain", 1);
+
+      cy.get(`[data-cy=incrementBtn]`).should("be.visible").as("incrementBtn");
+      cy.get("@incrementBtn").click();
+
+      cy.get("@couetr").should("contain", 2);
+
+      cy.get(`[data-cy=decrementBtn]`).should("be.visible").as("decrementBtn");
+      cy.get("@decrementBtn").click();
+
+      cy.get("@couetr").should("contain", 1);
+
+      cy.get(`[data-cy=completeBtn]`).should("be.visible").click();
+
+      cy.location("pathname").should("eq", "/");
+    });
+  });
 });
