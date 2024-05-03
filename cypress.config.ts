5100099035190cbfb5524b000d08c4c4e675cb8c
twy8939 <twy8939@naver.com>
2024-05-04 00:04:06 +0900

    feat : cypress cloud 설정

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "gpu1bc",
   e2e: {
     baseUrl: "http://localhost:5173/",
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 });
